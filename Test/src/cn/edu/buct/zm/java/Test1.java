7a0ccd2b84da5378aa9f2d81cc531353062e2bce
DaiLiang <dddlbj@163.com>
2014-03-31 22:30:00 +0800

    Test

--- Test/src/cn/edu/buct/zm/java/Test1.java
+++ Test/src/cn/edu/buct/zm/java/Test1.java
@@ -1,5 +1,7 @@
 package cn.edu.buct.zm.java;
-//测试
+
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 14-3-30.
  */
@@ -22,3 +24,29 @@ public class Test1 {
         }
     }
 }
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int m;
+//        int number1 = 1;
+//        int number2 = 1;
+//        int s = 0;
+//        System.out.println("请输入月份：");
+//        int month = scanner.nextInt();
+//        for (m = 3; m <= month; m++) {
+//            if (month <= 0) {
+//                System.out.println("输入值无意义！");
+//            } else if (month > 0 & month < 3) {
+//                s = 1;
+//            } else {
+//                s = number2;
+//                number2 = number1 + number2;
+//                number1 = s;
+//
+//            }
+//
+//
+//        }
+//        System.out.println("第" + month + "个月的兔子总量为:" + number2);
+//    }
+//}
