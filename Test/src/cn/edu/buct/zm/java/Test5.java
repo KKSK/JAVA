bd00f87b729e356a4b7900336056fbfcabc57015
DaiLiang <dddlbj@163.com>
2014-04-01 20:59:46 +0800

    Test

--- Test/src/cn/edu/buct/zm/java/Test5.java
+++ Test/src/cn/edu/buct/zm/java/Test5.java
@@ -7,20 +7,36 @@ import java.util.Scanner;
  */
 import java.util.*;
 import java.util.Scanner;
-public class Test5 {
-        public static void main(String[]args){
-            System.out.println("请输入成绩:");
-            Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            grade(n);
+//public class Test5 {
+//        public static void main(String[]args){
+//            System.out.println("请输入成绩:");
+//            Scanner in = new Scanner(System.in);
+//            int n = in.nextInt();
+//            grade(n);
+//        }
+//        //成绩计算器
+//        private static void grade(int n){
+//            if(n>100||n<0){
+//                System.out.println("输入无效!");
+//            }else{
+//                String str= (n>=90)?"分,属于A等":(n>=60)?"分,属于B等":"分,属于C等";
+//                System.out.println(n+str);
+//            }
+//        }
+//}
+
+    //同一方法
+    public class Test5 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请输入成绩：");
+        int n= scanner.nextInt();
+        if(n>100||n<0){
+            System.out.println("输入成绩无效，请重新输入！");
         }
-        //成绩计算器
-        private static void grade(int n){
-            if(n>100||n<0){
-                System.out.println("输入无效!");
-            }else{
-                String str= (n>=90)?"分,属于A等":(n>=60)?"分,属于B等":"分,属于C等";
-                System.out.println(n+str);
-            }
+        else {String s= (n>=90)?"属于A等":((n>=60)?"属于B等":"属于C等");
+            System.out.println(n+"分，"+s);
         }
+
+    }
     }
