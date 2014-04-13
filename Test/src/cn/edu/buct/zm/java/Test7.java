4c07ee2a52e7a01025296c24610e0152e37c5082
KKSK <dddlbj@163.com>
2014-04-13 14:33:53 +0800

    First

--- Test/src/cn/edu/buct/zm/java/Test7.java
+++ Test/src/cn/edu/buct/zm/java/Test7.java
@@ -12,27 +12,27 @@ public class Test7 {
         String p = scanner.nextLine();
         System.out.println(p.length());
         char[] c = p.toCharArray();
-        int sNumber = 0;
-        int nNumber = 0;
-        int cNumber = 0;
-        int kNumber = 0;
+        int sNumber = 0;//字母
+        int nNumber = 0;//数字
+        int cNumber = 0;//字符
+        int spaceNumber = 0;//空格
         int chineseNumber=0;
         for (int i = 0; i < p.length(); i++) {
             if (c[i] == 32) {
-                kNumber++;
+                spaceNumber++;
             }  else if ((c[i]>=48&&c[i]<=57)){
                 nNumber++;
             } else if ((c[i]>=65&&c[i]<=90)|(c[i]>=97&&c[i]<=122)){
                 sNumber++;
             }
-//            else if(c[i]>=4e00&c[i]<=9fA5){
+//            else if((c[i]>=4e00)&&(c[i]<=9fBB)){
 //                chineseNumber++;
 //            }
             else {
                 cNumber++;
             }
         }
-        System.out.println("输入字符中空格个数为:"+kNumber);
+        System.out.println("输入字符中空格个数为:"+spaceNumber);
         System.out.println("输入字符中字母个数为:"+sNumber);
         System.out.println("输入字符中数字个数为:"+nNumber);
         System.out.println("输入字符中字符个数为:"+cNumber);
