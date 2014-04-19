91bd013487242d4623a5b7ffacf942d2ca7d892b
KKSK <dddlbj@163.com>
2014-04-19 16:51:02 +0800

    First

---
+++ Test/src/cn/edu/buct/zm/java/Test19.java
@@ -0,0 +1,34 @@
+package cn.edu.buct.zm.java;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 14-4-19.
+ */
+public class Test19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入菱形行数：");
+        int row=scanner.nextInt();
+        System.out.println("输入菱形列数：");
+        int col=scanner.nextInt();
+String [][] star=new String[row][col];
+
+        for (int i = 0; i < star.length; i++) {
+            for (int j = 0; j < star[row].length; j++) {
+if (j<col/2){
+    System.out.println(star[row][col]=" ");
+    System.out.println(star[row][col]=" ");
+}else if (j==col/2){
+
+}
+
+                String[] strings = star[j];
+
+            }
+            String[] strings = star[i];
+
+        }
+
+                }
+            }
